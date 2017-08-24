2d3addc0fe4cd22ab562617ae0457c12c42678a5
Bilel Belwafi <bilel@garagosoftware.com>
2017-08-24 19:47:24 +0000

    PersonalOffersController.php edited online with Bitbucket

--- sm/app/Controller/PersonalOffersController.php
+++ sm/app/Controller/PersonalOffersController.php
@@ -7,11 +7,6 @@ App::uses('AppController', 'Controller');
  */
 class PersonalOffersController extends AppController {
 
-/**
- * index method
- *
- * @return void
- */
 	public function index() {
 		$this->PersonalOffer->recursive = 0;
 		$this->set('personalOffers', $this->paginate());
@@ -27,10 +22,12 @@ class PersonalOffersController extends AppController {
 
 		function subscribe($id=null)
 {
+  if ($this->request->is('put'))
+  {
   $this->loadModel('Regulation');
     $this->loadModel('Transaction');
+    $id=$this->request->query['id']
      $Regulations = $this->Regulation->find('first',array('conditions'=>['Regulation.id'=>$id]));
-
 $periode=$Regulations['Regulation']['period'];
 if($periode!='12')
   $message="$periode month";
@@ -41,39 +38,42 @@ else
       "Compte premium $message","action=subscribe&uid=$user_id&duration=$message");
   	if($url)
   		{
-  		//	$this->redirect($url);
   			$this->succes($id);
   		}
 }
-function succes($id)
-{
-  
-  $user_id=$this->Session->read('id');
-  $this->loadModel('Regulation');
-  $this->loadModel('Historical');
-   $this->loadModel('PersonalOffer');
-   $this->loadModel('Offer');
-  $Regulations = $this->Regulation->find('first',array('conditions'=>['Regulation.id'=>$id]));
-  $this->loadModel('Historical'); 
-$periode=$Regulations['Regulation']['period'];
-  $data=array('period'=>date("Y-m-d")."=>".date("Y-m-d",strtotime('+'.$periode.'month')),
-    'rabais'=>'1',
-    'regulation_id'=>$id,
-    'user_id'=>$user_id);
-  $this->Historical->create();
-  $this->Historical->save($data);
-  $Historical_id=$this->Historical->getLastInsertID();
- $offers=$this->Offer->find('all',array('conditions'=>['Offer.regulation_id'=>$id]));
- foreach ($offers as $offer) {
-  $data=['nombre'=>$offer['Offer']['nombre'],
-         'price_ligne'=>$offer['Offer']['price_ligne'],
-         'historical_id'=>$Historical_id,
-         'item_id'=>$offer['Offer']['items_id']
-        ];
-  $this->PersonalOffer->create();
-  $this->PersonalOffer->save($data);
-$this->redirect("../");
- }
 }
 
+function succes($id)
+  {
+    if(!($this->request))
+      {
+        $user_id=$this->Session->read('id');
+        $this->loadModel('Regulation');
+        $this->loadModel('Historical');
+         $this->loadModel('PersonalOffer');
+         $this->loadModel('Offer');
+        $Regulations = $this->Regulation->find('first',array('conditions'=>['Regulation.id'=>$id]));
+        $this->loadModel('Historical'); 
+      $periode=$Regulations['Regulation']['period'];
+        $data=array('period'=>date("Y-m-d")."=>".date("Y-m-d",strtotime('+'.$periode.'month')),
+          'rabais'=>'1',
+          'regulation_id'=>$id,
+          'user_id'=>$user_id);
+        $this->Historical->create();
+        $this->Historical->save($data);
+        $Historical_id=$this->Historical->getLastInsertID();
+       $offers=$this->Offer->find('all',array('conditions'=>['Offer.regulation_id'=>$id]));
+       foreach ($offers as $offer) 
+         {
+          $data=['nombre'=>$offer['Offer']['nombre'],
+                 'price_ligne'=>$offer['Offer']['price_ligne'],
+                 'historical_id'=>$Historical_id,
+                 'item_id'=>$offer['Offer']['items_id']
+                ];
+          $this->PersonalOffer->create();
+          $this->PersonalOffer->save($data);
+          $this->redirect("../");
+       }
+    }
+  }
 }
