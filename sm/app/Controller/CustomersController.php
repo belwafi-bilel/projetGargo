b5809dcf65993d3f9d87a2d72ec30946cd922fcd
Bilel Belwafi <bilel@garagosoftware.com>
2017-08-24 17:27:58 +0000

    CustomersController.php edited online with Bitbucket

--- sm/app/Controller/CustomersController.php
+++ sm/app/Controller/CustomersController.php
@@ -5,33 +5,62 @@ App::uses('AppController', 'Controller');
  *
  * @property Customer $Customer
  */
+
 class CustomersController extends AppController {
 
 
-	public function index($liste=null) {
-      if($liste){
-      $index=explode(',-,',$liste);
-      if(count($index)==2)
-      {
-		
-			$data=array('id'=>$index[0],'description'=>$index[1]);
-		if ($this->request->is('post') || $this->request->is('put')) 
-			$this->Customer->save($data);
-	}else if(count($index)==1)
+public function add()
+{
+	if ($this->request->is('post')||($this->request->is('put')))
 	{
-	$data=array('description'=>$index[0]);
-	$this->Customer->create();
+		$request=$this->request->query;
+ 	$data=array(
+ 	'description'=>$request['description'],
+ 	'langue'=>$request['langue']);
+ 	$this->Customer->create();
 	$this->Customer->save($data);
-	}else if(count($index)>2)
-	{
-$this->Customer->id = $index[0];
-$this->Customer->delete();
+	$this->Customer->recursive = 0;
+	$this->response->body(json_encode($this->paginate()));
+	return $this->response;
 	}
-	}
-
-		$this->Customer->recursive = 0;
-		$this->set('customers', $this->paginate());
-
 }
+public function edit()
+{
+	if ($this->request->is('put'))
+	{
+		$request=$this->request->query;
+ 	$data=array(
+ 		'id'=>$request['id'],
+ 	'description'=>$request['description'],
+ 	'langue'=>$request['langue']);
+ 	$this->Customer->create();
+	$this->Customer->save($data);
+	$this->Customer->recursive = 0;
+	$this->response->body(json_encode($this->paginate()));
+	return $this->response;
+	}
+}
+public function delete()
+{
+	if ($this->request->is('delete'))
+	{
+		$request=$this->request->query;
+ $this->Customer->id=$request['id'];
+ $this->Customer->delete();
+	$this->Customer->recursive = 0;
+	$this->response->body(json_encode($this->paginate()));
+	return $this->response;
+	}
+}
+public function view()
+{
+	if ($this->request->is('get'))
+		{
+		$sectors=$this->Customer->find('all');
+		$this->response->body(json_encode($sectors));
+		return $this->response;
+		}	
+}
+
 
 }
